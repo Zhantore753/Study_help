de49139dd7c3f188f66c9da87365468dc736aa47
Zhantore <753gg@mail.ru>
2020-10-06 16:28:18 +0600

    Модалка с  информацией по аккаунту

--- src/js/modules/settings.js
+++ src/js/modules/settings.js
@@ -3,12 +3,20 @@ const settings = () => {
         login = document.querySelector('.setting-login'),
         pass = document.querySelector('.setting-pass'),
         id = document.querySelector('.setting-id'),
-        btnShow = document.querySelector('.btn-show');
+        realId = localStorage.getItem('id'),
+        checkShow = document.querySelector('.check-show');
     name.value = localStorage.getItem('name');
     login.value = localStorage.getItem('login');
-    pass.value = localStorage.getItem('pass');
-    id.textContent = 'Уникальный идентификатор: ' + localStorage.getItem('id');
+    pass.value = (localStorage.getItem('pass')).replace(/\*/g, '*');
+    id.textContent = 'Уникальный идентификатор: ' + realId;
 
+    checkShow.addEventListener('click', () => {
+        if (checkShow.checked) {
+            pass.setAttribute('type', 'text');
+        } else {
+            pass.setAttribute('type', 'password');
+        }
+    });
 };
 
 export default settings;
